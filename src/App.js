608db6ec2e8cdfd181d88812326ad2537ecbb656
Belen Humbert <belenhumbert69@gmail.com>
2024-10-25 17:52:57 -0300

    agrego rutas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './pages/Home';
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ModificarPartido from './pages/ModificarPartido';
 function App() {
   return (
     <div>
       <Navbar/>
       <Routes>
         <Route path="/home" element={<Home/>}></Route>
+        <Route path="/modificar-partido" element={<ModificarPartido />} />
       </Routes>
       <Footer/>
     </div>
