e89d8e2ff90487d5b63225179dcf5744eac95553
Maria Rojas <marielene975jkm@gmail.com>
2024-11-25 15:37:54 -0300

    Subo el navbar del Admin

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ListarPartidos from './components/ListarPartidos';
+import ListarPartidosEstado from './components/ListarPartidosEstado';
 import ModificarPartido from './pages/ModificarPartido';
+import NavbarAdmin from './components/NavbarAdmin';
 function App() {
   return (
     <div>
       <Navbar/>
       <Routes>
+      <Route path="/admin" element={<NavbarAdmin/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
         <Route path="/modificar-partido" element={<ModificarPartido />} />
-        <Route path="/listar-partidos" element={<ListarPartidos />} />
+        <Route path="/listar-partidos" element={<ListarPartidos />} /> 
+        <Route path="/listar-partidos-por-estado" element={<ListarPartidosEstado/>} />
       </Routes>
       <Footer/>
     </div>
